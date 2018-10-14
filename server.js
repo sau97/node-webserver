dee9146c770662c6cdbec23c467642fb2b0f760b
sau97 <sa2550arora@gmail.com>
2018-10-14 18:55:04 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const hbs = require('hbs');
+var app = express();
+hbs.registerPartials(__dirname+'/views/partials')
+hbs.registerHelper('getCurrentYear',()=>{
+    return new Date().getFullYear();
+});
+hbs.registerHelper('screamIt',(text)=>text.toUpperCase())
+app.set('view engine','hbs');
+app.use(express.static(__dirname + '/public'))
+
+// app.use((req,res,next)=>{
+//     res.render('maintainance.hbs');
+//     next();
+// });
+app.use(express.static(__dirname + '/public'))
+app.get('/about',(req,res)=>{
+    res.render('about.hbs',{
+        pageTitle:'About Page dynamically'
+    });
+});
+app.get('/',(req,res)=>{
+    res.render('home.hbs',{
+        pageTitle:'HOME dynamically',
+        welcomeMessage:'welcome home'
+    });
+});
+app.get('/bad',(req,res)=>{
+    res.send({
+        errorMessage:'unable to handle request'
+    });
+});
+app.listen(3000,()=>{
+    console.log('on port 3000');
+});
